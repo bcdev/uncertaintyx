7f92d082a3d8bf85357d7ea640f8a37736f14170
Ralf Quast <ralf.quast@brockmann-consult.de>
2026-04-07 17:09:13 +0200

    New: test data for QAA

--- test/resources/__init__.py
+++ test/resources/__init__.py
@@ -1,2 +1,30 @@
 #  Copyright (c) Brockmann Consult GmbH, 2026.
 #  License: MIT
+
+"""
+This package provides test data.
+
+The following files provide excerpts from Lee et al. (2015):
+
+- a.csv: total absorption
+- adg.csv: absorption by detritus and gelbstoff
+- aph.csv: absorption by phytoplankton
+- rrr.csv: remote sensing reflectance
+
+The following data were kindly provided by Zhong Ping Lee (2026,
+personal communication):
+
+- fig2.csv: plot data for Lee et al. (2010, Figure 2)
+- fig3.csv: plot data for Lee et al. (2010, Figure 3)
+
+References:
+
+Lee et al. (2010). An Update of the Quasi-Analytical Algorithm.
+https://www.ioccg.org/groups/Software_OCA/QAA_v5.pdf.
+
+Lee et al. (2014). An Update of the Quasi-Analytical Algorithm.
+https://www.ioccg.org/groups/Software_OCA/QAA_v6_2014209.pdf
+
+Lee et al. (2015). Excel sheet with QAA v6 reference calculations.
+https://www.ioccg.org/groups/Software_OCA/QAA_v6.xlsm:
+"""
