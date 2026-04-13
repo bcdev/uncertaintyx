561ac66539368a52f94bb8f6682ecce6c132b593
Ralf Quast <ralf.quast@brockmann-consult.de>
2026-04-13 21:07:37 +0200

    Update: docstrings

--- uncertaintyx/fit/eiv/jax.py
+++ uncertaintyx/fit/eiv/jax.py
@@ -1,5 +1,6 @@
 #  Copyright (c) Brockmann Consult GmbH, 2026.
 #  License: MIT
+from typing import Any
 
 import jax
 import jax.numpy as jnp
@@ -15,19 +16,16 @@ from ...m.jax import vec_x
 
 
 @jax.jit(static_argnums=0)
-def evm_fit(f, p, x, y, ux, uy, max_iter: int = 100, tol_g: float = 1.0e-06):
-    optim = optax.lbfgs()
-    state = optim.init(p)
+def evm_fit(f, p, x, y, ux, uy, max_iter: int = 100, tol_g: Any = 1.0e-06):
+    """
+    Implementation of the effective variance method with L-BFGS.
+    """
 
     def loss(p: Array):
         """The loss function."""
         return 0.5 * jnp.sum(
             (vec_x(f, p, x) - y) ** 2 / (uy**2 + (jac_x(f, p, x) * ux) ** 2)
         )
-
-    cost_and_grad = optax.value_and_grad_from_state(loss)
-    cost, grad = cost_and_grad(p, state=state)
-    init = 0, p, state, cost, grad, False
 
     def cond(carry):
         """The function to check the loop condition."""
@@ -45,10 +43,20 @@ def evm_fit(f, p, x, y, ux, uy, max_iter: int = 100, tol_g: float = 1.0e-06):
         converged = jnp.linalg.norm(grad, ord=jnp.inf) < tol_g  # noqa
         return i + 1, popt, state, cost, grad, converged
 
+    optim = optax.lbfgs()
+    state = optim.init(p)
+    cost_and_grad = optax.value_and_grad_from_state(loss)
+    cost, grad = cost_and_grad(p, state=state)
+    init = 0, p, state, cost, grad, False
+
     return jax.lax.while_loop(cond, body, init)  # noqa
 
 
 class EVM(Fitting):
+    """
+    ODR-like regression using the effective variance method (EVM).
+    """
+
     def fit(
         self,
         f: M,
@@ -60,6 +68,18 @@ class EVM(Fitting):
         max_iter: int = 100,
         **kwargs,
     ) -> Result:
+        r"""
+        Fits the parameters of a model function to :math:`M`
+        samples :math:`(x_i, y_i)` of data.
+
+        :param f: The model function.
+        :param x: Samples :math:`X \in \mathbb{R}^{M \times m}`.
+        :param y: Samples :math:`Y \in \mathbb{R}^{M \times n}`.
+        :param ux: Uncertainties :math:`u(X) \in \mathbb{R}^{M \times m}`.
+        :param uy: Uncertainties :math:`u(Y) \in \mathbb{R}^{M \times n}`.
+        :param max_iter: The maximum number of iterations conducted.
+        :returns: The fit result.
+        """
         i, popt, state, cost, g, converged = evm_fit(
             f.f,
             jnp.asarray(f.estimate(x, y)),
