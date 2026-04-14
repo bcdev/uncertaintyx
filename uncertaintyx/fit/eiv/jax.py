f581a90179da6565c62a52b0ced3bb014e732dc7
Ralf Quast <ralf.quast@brockmann-consult.de>
2026-04-14 17:58:09 +0200

    Update: facilitate full sample covariance

--- uncertaintyx/fit/eiv/jax.py
+++ uncertaintyx/fit/eiv/jax.py
@@ -5,6 +5,7 @@ from typing import Callable
 
 import jax
 import jax.numpy as jnp
+import jax.scipy as jcp
 import numpy as np
 import optax
 from jax import Array
@@ -12,11 +13,10 @@ from jax import Array
 from ...interface.core import Fitting
 from ...interface.core import M
 from ...interface.core import Result
-from ...m.jax import jac_x
-from ...m.jax import vec_x
+from ...m.jax import jac
 
 
-@jax.jit(static_argnums=0)
+@jax.jit(static_argnums=(0,))
 def evm(
     f: Callable[[Array, Array], Array],
     p: Array,
@@ -36,23 +36,72 @@ def evm(
     :param p: Parameters :math:`p \in \mathbb{R}^{k}`.
     :param x: Samples :math:`X \in \mathbb{R}^{M \times m}`.
     :param y: Samples :math:`Y \in \mathbb{R}^{M \times n}`.
-    :param ux: Uncertainties :math:`u(X) \in \mathbb{R}^{M \times m}`.
-    :param uy: Uncertainties :math:`u(Y) \in \mathbb{R}^{M \times n}`.
+    :param ux: Uncertainty :math:`U(X) \in \mathbb{R}^{M \times m}`.
+    :param uy: Uncertainty :math:`U(Y) \in \mathbb{R}^{M \times n}`.
     :param max_iter: The maximum number of iterations permitted.
     :param obj_g: The gradient objective to achieve.
     :returns: A tuple carrying the state of the optimization.
     """
 
+    def sample_loss(
+        p: Array, x: Array, y: Array, ux: Array, uy: Array
+    ) -> Array:
+        r"""
+        The sample loss function.
+
+        :param p: The parameters.
+        :param x: The sample :math:`x \in \mathbb{R}^{m}`.
+        :param y: The sample :math:`y \in \mathbb{R}^{n}`.
+        :param ux: :math:`U(x) \in \mathbb{R}^{m \times m}`.
+        :param uy: :math:`U(y) \in \mathbb{R}^{n \times n}`.
+
+        :returns: The loss.
+        """
+
+        def up_full(d: int, u: Array) -> Array:
+            """Uncertainty propagation."""
+            dims = tuple(range(-d, 0))
+            return jnp.tensordot(
+                jnp.tensordot(g, u, axes=(dims, dims)), g, axes=(dims, dims)
+            )
+
+        def up_diag(d: int, u: Array) -> Array:
+            """Uncertainty propagation."""
+            dims = tuple(range(-d, 0))
+            return jnp.sum(
+                jnp.tensordot(g, u, axes=(dims, dims)) * g, axis=dims
+            )
+
+        g = jac(f, 1, y.size < x.size)(p, x)
+        d = jnp.reshape(f(p, x) - y, -1)
+        if ux.shape == x.shape and uy.shape == y.shape:
+            v = jnp.reshape(uy + up_diag(x.ndim, ux), -1)  # noqa: N806
+            b = d / v
+        elif uy.shape == y.shape:
+            V = jnp.diag(uy.reshape(-1)) + jnp.reshape(  # noqa: N806
+                up_full(x.ndim, ux), d.shape + d.shape
+            )
+            L = jcp.linalg.cho_factor(V)  # noqa: N806
+            b = jcp.linalg.cho_solve(L, d)  # noqa
+        else:
+            V = jnp.reshape(  # noqa: N806
+                uy + up_full(x.ndim, ux), d.shape + d.shape
+            )
+            L = jcp.linalg.cho_factor(V)  # noqa: N806
+            b = jcp.linalg.cho_solve(L, d)  # noqa
+        return 0.5 * jnp.sum(d * b)
+
     def loss(p: Array) -> Array:
         """
-        The loss function.
+        The batch loss function.
 
         :param p: The parameters.
         :returns: The loss.
         """
-        return 0.5 * jnp.sum(
-            (vec_x(f, p, x) - y) ** 2 / (uy**2 + (jac_x(f, p, x) * ux) ** 2)
+        losses = jax.vmap(sample_loss, in_axes=(None, 0, 0, 0, 0))(
+            p, x, y, ux, uy
         )
+        return jnp.sum(losses)
 
     def cond(carry: tuple[int, Array, Any, Array, Array, bool]) -> Array:
         """
@@ -141,8 +190,8 @@ class EIV(Fitting):
             jnp.asarray(f.estimate(x, y)),
             jnp.asarray(x),
             jnp.asarray(y),
-            jnp.asarray(ux) if ux is not None else jnp.ones_like(x),
-            jnp.asarray(uy) if uy is not None else jnp.ones_like(y),
+            jnp.asarray(ux * ux) if ux is not None else jnp.ones_like(x),
+            jnp.asarray(uy * uy) if uy is not None else jnp.ones_like(y),
             max_iter,
         )
         popt = np.asarray(popt)
