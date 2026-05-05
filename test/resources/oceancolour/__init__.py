27d1d1a0f4fd849f815d0f36184bc87785841108
Ralf Quast <ralf.quast@brockmann-consult.de>
2026-05-05 12:02:38 +0200

    Update: move oceancolour test resources

---
+++ test/resources/oceancolour/__init__.py
@@ -0,0 +1,33 @@
+#  Copyright (c) Brockmann Consult GmbH, 2026.
+#  License: MIT
+"""
+Test data package.
+
+The following files are provided:
+
+.. csv-table::
+   :header: Name, Description, Unit, Reference
+
+   a.csv, Total absorption, m-1, Lee et al. (2015)
+   adg.csv, Absorption by detritus and gelbstoff, m-1, Lee et al. (2015)
+   aph.csv, Absorption by phytoplankton, m-1, Lee et al. (2015)
+   owt.csv, Optical water type classes, Jackson et al. (2017)
+   rrs.csv, Remote sensing reflectance, sr-1, Lee et al. (2015)
+   fig2.csv, Plot data,, Lee et al. (2010; Figure 2)
+   fig3.csv, Plot data,, Lee et al. (2010; Figure 3)
+
+References:
+
+Jackson et al. (2017). An improved optical classification scheme for
+the Ocean Colour Essential Climate Variable and its applications,
+https://dx.doi.org/10.1016/j.rse.2017.03.036.
+
+Lee et al. (2010). An Update of the Quasi-Analytical Algorithm.
+https://www.ioccg.org/groups/Software_OCA/QAA_v5.pdf.
+
+Lee et al. (2014). An Update of the Quasi-Analytical Algorithm.
+https://www.ioccg.org/groups/Software_OCA/QAA_v6_2014209.pdf
+
+Lee et al. (2015). Excel sheet with QAA v6 reference calculations.
+https://www.ioccg.org/groups/Software_OCA/QAA_v6.xlsm:
+"""
