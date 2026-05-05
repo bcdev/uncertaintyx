b1b207c1fe4f3627a05db70cf65a9d08c6a7abb2
Ralf Quast <ralf.quast@brockmann-consult.de>
2026-05-05 11:19:40 +0200

    New: test examples from GUM, Supplement 2

---
+++ test/gum/test_example_9_4.py
@@ -0,0 +1,104 @@
+#  Copyright (c) Brockmann Consult GmbH, 2026.
+#  License: MIT
+
+import unittest
+
+import jax.numpy as jnp
+import numpy as np
+from jax import Array
+
+import uncertaintyx.f.jax as tyx
+from test.gum import to_cor
+
+C = jnp.asarray([[1.0, 0.0, 1.0], [0.0, 1.0, 1.0]])
+"""The sensitivity matrix."""
+
+
+class Impedance(tyx.ToF):
+    """
+    The impedance measurement model.
+    """
+
+    def __init__(self):
+        def f(x: Array) -> Array:
+            """The impedance measurement model"""
+            V, I, phi = x  # noqa: N806
+            Z = V / I  # noqa: N806
+            R = Z * jnp.cos(phi)  # noqa: N806
+            X = Z * jnp.sin(phi)  # noqa: N806
+            return jnp.stack(
+                [
+                    R,
+                    X,
+                    Z,
+                ]
+            )
+
+        super().__init__(f)
+
+
+class ExampleTest(unittest.TestCase):
+    """
+    Tests an impedance measurement (GUM Supplement 2, Example 9.4,
+    https://doi.org/10.59161/JCGM102-2011).
+
+    Tables 9 and 10 list rounded numbers which are too inaccurate
+    to reproduce the test. Mean values and uncertainty tensor are
+    therefore computed (to machine precision) from the measured
+    values by means of equations listed under 9.4.2.2.
+
+    Round-off error obviously affected some figures listed in
+    Table 11.
+    """
+
+    def test_9_4_2(self):
+        measured = np.array(
+            [
+                [5.007, 19.663e-03, 1.0456],
+                [4.994, 19.639e-03, 1.0438],
+                [5.005, 19.640e-03, 1.0468],
+                [4.990, 19.685e-03, 1.0428],
+                [4.999, 19.678e-03, 1.0433],
+                [4.999, 19.661e-03, 1.0445],
+            ]
+        )
+        X = np.broadcast_to(np.mean(measured, axis=0), (1, 3))  # noqa: N806
+        U = np.broadcast_to(  # noqa: N806
+            np.tensordot(measured - X, measured - X, ([0], [0])) / 30.0,
+            (1, 3, 3),
+        )
+        f = Impedance()
+
+        Y = f.eval(X)  # noqa: N806
+        self.assertAlmostEqual(127.732, Y[0, 0], places=2)  # round-off
+        self.assertAlmostEqual(219.847, Y[0, 1], places=3)
+        self.assertAlmostEqual(254.260, Y[0, 2], places=3)
+
+        G = f.jac(X)  # noqa: N806
+        V, I, phi = X[0]  # noqa: N806
+        self.assertAlmostEqual(np.cos(phi) / I, G[0, 0, 0])
+        self.assertAlmostEqual(-V * np.cos(phi) / I**2, G[0, 0, 1])
+        self.assertAlmostEqual(-V * np.sin(phi) / I, G[0, 0, 2])
+        self.assertAlmostEqual(np.sin(phi) / I, G[0, 1, 0])
+        self.assertAlmostEqual(-V * np.sin(phi) / I**2, G[0, 1, 1])
+        self.assertAlmostEqual(V * np.cos(phi) / I, G[0, 1, 2])
+        self.assertAlmostEqual(1.0 / I, G[0, 2, 0])
+        self.assertAlmostEqual(-V / I**2, G[0, 2, 1])
+        self.assertAlmostEqual(0.0, G[0, 2, 2])
+
+        U = f.lpu(X, U)  # noqa: N806
+        self.assertAlmostEqual(0.058, np.sqrt(U[0, 0, 0]), places=3)
+        self.assertAlmostEqual(0.241, np.sqrt(U[0, 1, 1]), places=3)
+        self.assertAlmostEqual(0.193, np.sqrt(U[0, 2, 2]), places=3)
+
+        R = to_cor(1, U)  # noqa: N806
+        self.assertAlmostEqual(-0.588, R[0, 0, 1], places=3)
+        self.assertAlmostEqual(-0.485, R[0, 0, 2], places=3)
+        self.assertAlmostEqual(-0.588, R[0, 1, 0], places=3)
+        self.assertAlmostEqual(0.00749, 1.0 - R[0, 1, 2], places=5)
+        self.assertAlmostEqual(-0.485, R[0, 2, 0], places=3)
+        self.assertAlmostEqual(0.00749, 1.0 - R[0, 2, 1], places=5)
+
+
+if __name__ == "__main__":
+    unittest.main()
