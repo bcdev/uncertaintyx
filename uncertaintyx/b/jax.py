bd2a3c5359fa05a17d2c51a3c2c1b504f42c9095
Ralf Quast <ralf.quast@brockmann-consult.de>
2026-05-12 17:22:58 +0200

    Update: delete unused code

--- uncertaintyx/b/jax.py
+++ uncertaintyx/b/jax.py
@@ -6,20 +6,6 @@ from jax import Array
 from jax.scipy.special import gammaln
 
 
-def b_basis_poly(i: int, n: int, x: Array) -> Array:
-    r"""
-    Evaluates the :math:`i`-th Bernstein basis polynomial
-    of degree :math:`n`.
-
-    :param i: The number of the basis polynomial
-    :param n: The degree of the basis polynomial.
-    :param x: The input :math:`x \in \mathbb{R}^{m}`.
-    :returns: The basis polynomial :math:`B_{i, n}(x) \in \mathbb{R}^{m}`.
-    """
-    binom = jnp.exp(gammaln(n + 1) - (gammaln(i + 1) + gammaln(n - i + 1)))
-    return binom * jnp.power(x, i) * jnp.power(1.0 - x, n - i)
-
-
 def _binom(i, n) -> Array:
     return jnp.exp(gammaln(n + 1) - (gammaln(i + 1) + gammaln(n - i + 1)))
 
