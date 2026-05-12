32101965c467657d30ee2f44c233626b367bcb03
Ralf Quast <ralf.quast@brockmann-consult.de>
2026-05-12 17:08:53 +0200

    Update: Bernstein polynomials

--- test/b/test_b_jax.py
+++ test/b/test_b_jax.py
@@ -3,10 +3,12 @@
 
 import unittest
 
+import jax
 import jax.numpy as jnp
 import numpy as np
 
 from uncertaintyx.b.jax import b_basis
+from uncertaintyx.b.jax import b_poly
 
 
 class BasisTest(unittest.TestCase):
@@ -64,6 +66,51 @@ class BasisTest(unittest.TestCase):
         self.assertTrue(np.allclose(y[:-1, -1], 0.0))
         self.assertTrue(np.allclose(y[-1:, -1], 1.0))
         self.assertTrue(np.allclose(np.sum(y, axis=0), 1.0))
+
+
+class PolyTest(unittest.TestCase):
+    """Tests Bernstein polynomials."""
+
+    def test_b_poly_n(self):
+        m = 5
+        n = 5
+        b = jnp.ones(n + 1)
+        x = jnp.linspace(0.0, 1.0, m)
+        y = b_poly(b, x)
+
+        self.assertEqual((m,), y.shape)
+        self.assertTrue(np.allclose(y, 1.0))
+
+        b = jnp.linspace(1.0, 2.0, n + 1)
+        y = b_poly(b, x)
+
+        self.assertEqual((m,), y.shape)
+        self.assertTrue(np.allclose(y, 1.0 + x))
+
+    def test_b_grad_n(self):
+        def b_poly_sum(b, x):
+            """To test the gradient."""
+            return jnp.sum(b_poly(b, x))
+
+        @jax.jit
+        def b_poly_grad(b, x):
+            """To test the gradient."""
+            return jax.grad(b_poly_sum, argnums=1)(b, x)
+
+        m = 5
+        n = 5
+        b = jnp.ones(n + 1)
+        x = jnp.linspace(0.0, 1.0, m)
+        g = b_poly_grad(b, x)
+
+        self.assertEqual((m,), g.shape)
+        self.assertTrue(np.allclose(g, 0.0))
+
+        b = jnp.linspace(1.0, 2.0, n + 1)
+        g = b_poly_grad(b, x)
+
+        self.assertEqual((m,), g.shape)
+        self.assertTrue(np.allclose(g, 1.0))
 
 
 if __name__ == "__main__":
--- uncertaintyx/b/jax.py
+++ uncertaintyx/b/jax.py
@@ -6,7 +6,7 @@ from jax import Array
 from jax.scipy.special import gammaln
 
 
-def b_poly(i: int, n: int, x: Array) -> Array:
+def b_basis_poly(i: int, n: int, x: Array) -> Array:
     r"""
     Evaluates the :math:`i`-th Bernstein basis polynomial
     of degree :math:`n`.
@@ -20,6 +20,39 @@ def b_poly(i: int, n: int, x: Array) -> Array:
     return binom * jnp.power(x, i) * jnp.power(1.0 - x, n - i)
 
 
+def _binom(i, n) -> Array:
+    return jnp.exp(gammaln(n + 1) - (gammaln(i + 1) + gammaln(n - i + 1)))
+
+
+_b_basis = jax.custom_jvp(
+    lambda n, x: (
+        lambda i: (
+            _binom(i, n)
+            * jnp.power(x[jnp.newaxis, :], i)
+            * jnp.power(1.0 - x[jnp.newaxis, :], n - i)
+        )
+    )(jnp.arange(n + 1)[:, jnp.newaxis]),
+    nondiff_argnums=(0,),
+)
+
+
+def _b_basis_jvp(n, primals, tangents):
+    (x,) = primals
+    (t,) = tangents
+
+    basis = _b_basis(n, x)
+    if n == 0:
+        return basis, jnp.zeros_like(basis)
+    lower = _b_basis(n - 1, x)
+    jac_x = n * (
+        jnp.pad(lower, ((1, 0), (0, 0))) - jnp.pad(lower, ((0, 1), (0, 0)))
+    )
+    return basis, jac_x * t[jnp.newaxis, :]
+
+
+_b_basis.defjvp(_b_basis_jvp)
+
+
 @jax.jit(static_argnums=(0,))
 def b_basis(n: int, x: Array) -> Array:
     r"""
@@ -29,4 +62,16 @@ def b_basis(n: int, x: Array) -> Array:
     :param x: The input :math:`x \in \mathbb{R}^{m}`.
     :returns: The basis :math:`B_{n}(x) \in \mathbb{R}^{(n + 1) \times m}`.
     """
-    return jax.vmap(lambda i: b_poly(i, n, x))(jnp.arange(n + 1))
+    return _b_basis(n, x)
+
+
+@jax.jit
+def b_poly(b: Array, x: Array) -> Array:
+    r"""
+    Evaluates a Bernstein polynomial.
+
+    :param b: The Bernstein coefficients :math:`b \in \mathbb{R}^{(n + 1)}`.
+    :param x: The input :math:`x \in \mathbb{R}^{m}`.
+    :returns: The polynomial :math:`B(x) \in \mathbb{R}^{(n + 1) \times m}`.
+    """
+    return jnp.dot(b, b_basis(b.shape[-1] - 1, x))
