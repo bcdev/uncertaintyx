35adc29a492f9f7cf469fe3c987d03a3ea33d5d7
R. Quast <ralf.quast@brockmann-consult.de>
2026-05-14 10:07:51 +0200

    Refactor Bernstein basis functions to use degree k

--- uncertaintyx/b/jax.py
+++ uncertaintyx/b/jax.py
@@ -6,24 +6,27 @@ from jax import Array
 from jax.scipy.special import gammaln
 
 
-def _binom(i, n) -> Array:
-    """Returns the binomial coefficients for the whole basis."""
-    return jnp.exp(gammaln(n + 1) - (gammaln(i + 1) + gammaln(n - i + 1)))
+def _binom(i, k) -> Array:
+    """
+    Returns the binomial coefficients for the Bernstein basis
+    of degree :math:`k`.
+    """
+    return jnp.exp(gammaln(k + 1) - (gammaln(i + 1) + gammaln(k - i + 1)))
 
 
 _b_basis = jax.custom_jvp(
-    lambda n, x: (
+    lambda k, x: (
         lambda i: (
-            _binom(i, n)
+            _binom(i, k)
             * jnp.power(x[jnp.newaxis, :], i)
-            * jnp.power(1.0 - x[jnp.newaxis, :], n - i)
+            * jnp.power(1.0 - x[jnp.newaxis, :], k - i)
         )
-    )(jnp.arange(n + 1)[:, jnp.newaxis]),
+    )(jnp.arange(k + 1)[:, jnp.newaxis]),
     nondiff_argnums=(0,),
 )
 
 
-def _b_basis_jvp(n, inputs: tuple[Array], perturbations: tuple[Array]):
+def _b_basis_jvp(k, inputs: tuple[Array], perturbations: tuple[Array]):
     r"""
     Custom forward-mode differentiation (JVP) for the Bernstein basis.
 
@@ -32,13 +35,13 @@ def _b_basis_jvp(n, inputs: tuple[Array], perturbations: tuple[Array]):
 
     .. math::
 
-        G_{x} B_{i,n}(x) =
-        n \left(B_{i-1, n-1}(x) - B_{i, n-1}(x)\right) .
+        G_{x} B_{i,k}(x) =
+        k \left(B_{i-1, k-1}(x) - B_{i, k-1}(x)\right) .
 
     In the context of automatic differentiation, inputs are termed
     `primals` while perturbations are termed `tangents`.
 
-    :param n: The degree of the basis.
+    :param k: The degree of the basis.
     :param inputs: A tuple containing the :math:`x \in \mathbb{R}^{m}`.
     :param perturbations: A tuple containing the perturbation for :math:`x`.
     :returns: A tuple containing the basis and its perturbation.
@@ -46,14 +49,14 @@ def _b_basis_jvp(n, inputs: tuple[Array], perturbations: tuple[Array]):
     (x,) = inputs
     (d,) = perturbations
 
-    if n == 0:
-        basis = _b_basis(n, x)
+    if k == 0:
+        basis = _b_basis(k, x)
         return basis, jnp.zeros_like(basis)
 
-    basis_minus = _b_basis(n - 1, x)
+    basis_minus = _b_basis(k - 1, x)
     a = jnp.pad(basis_minus, ((1, 0), (0, 0)))
     b = jnp.pad(basis_minus, ((0, 1), (0, 0)))
-    g = n * (a - b)
+    g = k * (a - b)
     basis = b + (a - b) * x[jnp.newaxis, :]
     basis_perturbation = g * d[jnp.newaxis, :]
 
@@ -64,15 +67,15 @@ _b_basis.defjvp(_b_basis_jvp)
 
 
 @jax.jit(static_argnums=(0,))
-def b_basis(n: int, x: Array) -> Array:
+def b_basis(k: int, x: Array) -> Array:
     r"""
     Evaluates the Bernstein basis of degree :math:`n`.
 
-    :param n: The degree of the basis.
+    :param k: The degree of the basis.
     :param x: The input :math:`x \in \mathbb{R}^{m}`.
-    :returns: The basis :math:`B_{n}(x) \in \mathbb{R}^{(n + 1) \times m}`.
+    :returns: The basis :math:`B_{k}(x) \in \mathbb{R}^{(k + 1) \times m}`.
     """
-    return _b_basis(n, x)
+    return _b_basis(k, x)
 
 
 @jax.jit
